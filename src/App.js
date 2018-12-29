e6029c1641920ec55a77f76a340d65f3d9f7a862
Christopher Gonzales <cm.gonzales@yahoo.com>
2018-12-29 12:12:56 -0800

    first comit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    console.log("constructor")
+    this.state = {
+      name: ""
+    }
+  }
+  
+    componentWillMount(){
+      console.log("did mount")
+      
+      
+    }
+  
   render() {
+
+    var name =  this.state.name
+    console.log("render")
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <input type="text"/>
+          <h1></h1>
         </header>
       </div>
     );
