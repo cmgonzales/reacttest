ce78bb5f9ac9f1629c2a0725340f6ad5395f5856
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-01-02 18:26:40 -0800

    added handle events

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
 import React, { Component } from 'react';
 
+var x = ''
+var value = ""
+
 class App extends Component {
   constructor(props) {
     super(props)
 
     console.log("constructor")
+  
     this.state = {
-      value: ""
+      value: "",
+      name: ""
     }
-      this.handlchange = this.handlechange.bind(this)
-    
-  }
-  
+    this.handlechange = this.handlechange.bind(this)
+    this.random = this.random.bind(this)
 
-    handlechange(e){
-    this.setState({value: e.target.value})
-    }
+
+  }
+
+  handlechange(e) {
+    this.setState({ value: e.target.value })
+
+  }
+
+ random(){
+
+    x = Math.floor((Math.random() * 10) + 1)
+
+   this.setState({
+     name: x
+   })
+
+ if (x == value){
+
+
+  console.log("test")
+ }
   
+}
+
   render() {
 
-
-    console.log("render")
     return (
       <div className="App">
-          <label>
-            name
-          <input type="text" value = {this.state.value} onChange = {this.handlechange}  />
+        <label>
+          <h2>Please pick a number between 1 - 10</h2>
+          <input type="number" value={this.state.value} onChange={this.handlechange} />
+          <button onClick = {this.random} >submit</button>
 
-          <h1 value = {this.state.value}></h1>
-          </label>
+          <h1>{this.state.value}</h1>
+          <h2>{this.state.name}</h2>
+        </label>
       </div>
     );
   }
